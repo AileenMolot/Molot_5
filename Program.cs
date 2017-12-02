03c7d61b96b6614ada901e09db4d88cfc2c0f8b8
Aileen <aileenmolot@gmail.com>
2017-12-02 23:09:07 +0200

    Task 1 of hometask resolved, debug needed

--- Program.cs
+++ Program.cs
@@ -5,27 +5,43 @@ namespace Molot_5
     class MyClass
     {
         private int[] _array;
+        // private int _maxIndex;
         public int Length { get; }
-
-        public MyClass (int length)
+        public int MinIndex { get; }
+        public int MaxIndex { get; }
+       
+        public MyClass (int length,int minIndex)
         {
             this.Length = length;
+            this.MinIndex = minIndex;
+            this.MaxIndex = this.MinIndex + this.Length - 1;
             this._array = new int[this.Length]; 
         }
-        public void SetValue(int index, int expValue)
-        {
-            this._array[index] = expValue;
-        }
+
         public int this[int index]
         {
             get
             {
-                return this._array[index];
+                if (index < this.MinIndex || index > this.MaxIndex)
+                    throw new IndexOutOfRangeException ( String.Format("This index ({0}) is out of range from {1} to {2}.",index, this.MinIndex, this.MaxIndex) );
+
+                return this._array[index-this.MinIndex];
             }
             set
             {
-                this._array[index] = value;
+                if (index < this.MinIndex || index > this.MaxIndex)
+                    throw new IndexOutOfRangeException ( String.Format("This index ({0}) is out of range from {1} to {2}.",index, this.MinIndex, this.MaxIndex) );
+
+                this._array[index-this.MinIndex] = value;
             }
+        }
+
+        public override string ToString()
+        {
+            string strArr = "";
+            foreach (int el in this._array)
+                strArr = strArr + el + ", ";
+            return "{"+ strArr.Substring(0, strArr.Length - 2) + "}";
         }
     }
 
@@ -34,22 +50,23 @@ namespace Molot_5
     {
         static void Main(string[] args)
         {
-            int x = 4;
+            try
+            {
+                int arrayLength = 0;
+                int arrayMinIndex = 7;
+                MyClass arr = new MyClass(arrayLength, arrayMinIndex);
+                // a[11] = 32;
+                // int y = a[11];
+                for (int i = arr.MinIndex; i <= arr.MaxIndex; i++)
+                    arr[i] = i;
 
-            //Console.WriteLine("Insert value");
-            //int lenght = Convert.ToInt32(Console.ReadLine ());
-            int[] list = new int[3];
-            list[1] = x;
-            int y = list[1];
-            //for (int k = 0; k<lenght; k++)
-            //    list[k] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(arr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine (ex.Message);
+            }
 
-            MyClass a = new MyClass(x);
-            Console.WriteLine(a.Length);
-            //a.SetValue(1, 42);
-            a[1] = 32;
-            y = a[1];
-            Console.WriteLine(y);
         }
     }
 }
